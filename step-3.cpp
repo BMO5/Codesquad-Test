fd920a7fe50c5c99f56503814d52fb76cdc033c7
BMO5 <b38238496@gmail.com>
2020-12-13 02:31:03 +0900

    rotate 함수 추가
    6가지의 방향으로 회전하는 함수 추가

--- step-3.cpp
+++ step-3.cpp
@@ -26,6 +26,144 @@ void printCube()
         cout << "\t" << cubeL[i][0] << " " << cubeL[i][1] << " " << cubeL[i][2] << endl;
 }
 
+void rotateF(string direction)
+{
+    char temp[3] = {};
+    if (direction == "F")
+        for (int i = 0; i < 3; i++)
+        {
+            temp[i] = cubeU[2][i];
+            cubeU[2][i] = cubeM1[2 - i][2];
+            cubeM1[2 - i][2] = cubeL[0][2 - i];
+            cubeL[0][2 - i] = cubeM3[i][0];
+            cubeM3[i][0] = temp[i];
+        }
+    else
+        for (int i = 0; i < 3; i++)
+        {
+            temp[i] = cubeU[2][i];
+            cubeU[2][i] = cubeM3[i][0];
+            cubeM3[i][0] = cubeL[0][2 - i];
+            cubeL[0][2 - i] = cubeM1[2 - i][2];
+            cubeM1[2 - i][2] = temp[i];
+        }
+}
+
+void rotateR(string direction)
+{
+    char temp[3] = {};
+    if (direction == "R")
+        for (int i = 0; i < 3; i++)
+        {
+            temp[i] = cubeU[i][2];
+            cubeU[i][2] = cubeM2[i][2];
+            cubeM2[i][2] = cubeL[i][2];
+            cubeL[i][2] = cubeM4[2 - i][0];
+            cubeM4[2 - i][0] = temp[i];
+        }
+    else
+        for (int i = 0; i < 3; i++)
+        {
+            temp[i] = cubeU[i][2];
+            cubeU[i][2] = cubeM4[2 - i][0];
+            cubeM4[2 - i][0] = cubeL[i][2];
+            cubeL[i][2] = cubeM2[i][2];
+            cubeM2[i][2] = temp[i];
+        }
+}
+
+void rotateU(string direction)
+{
+    char temp[3] = {};
+    if (direction == "U")
+        for (int i = 0; i < 3; i++)
+        {
+            temp[i] = cubeM1[0][i];
+            cubeM1[0][i] = cubeM2[0][i];
+            cubeM2[0][i] = cubeM3[0][i];
+            cubeM3[0][i] = cubeM4[0][i];
+            cubeM4[0][i] = temp[i];
+        }
+    else
+        for (int i = 0; i < 3; i++)
+        {
+            temp[i] = cubeM1[0][i];
+            cubeM1[0][i] = cubeM4[0][i];
+            cubeM4[0][i] = cubeM3[0][i];
+            cubeM3[0][i] = cubeM2[0][i];
+            cubeM2[0][i] = temp[i];
+        }
+}
+
+void rotateB(string direction)
+{
+    char temp[3] = {};
+    if (direction == "B")
+        for (int i = 0; i < 3; i++)
+        {
+            temp[i] = cubeU[0][i];
+            cubeU[0][i] = cubeM3[i][2];
+            cubeM3[i][2] = cubeL[2][2 - i];
+            cubeL[2][2 - i] = cubeM1[2 - i][0];
+            cubeM1[2 - i][0] = temp[i];
+        }
+    else
+        for (int i = 0; i < 3; i++)
+        {
+            temp[i] = cubeU[0][i];
+            cubeU[0][i] = cubeM1[2 - i][0];
+            cubeM1[2 - i][0] = cubeL[2][2 - i];
+            cubeL[2][2 - i] = cubeM3[i][2];
+            cubeM3[i][2] = temp[i];
+        }
+}
+
+void rotateL(string direction)
+{
+    char temp[3] = {};
+    if (direction == "L")
+        for (int i = 0; i < 3; i++)
+        {
+            temp[i] = cubeU[2 - i][0];
+            cubeU[2 - i][0] = cubeM4[i][2];
+            cubeM4[i][2] = cubeL[2 - i][0];
+            cubeL[2 - i][0] = cubeM2[2 - i][0];
+            cubeM2[2 - i][0] = temp[i];
+        }
+    else
+        for (int i = 0; i < 3; i++)
+        {
+            temp[i] = cubeU[2 - i][0];
+            cubeU[2 - i][0] = cubeM2[2 - i][0];
+            cubeM2[2 - i][0] = cubeL[2 - i][0];
+            cubeL[2 - i][0] = cubeM4[i][2];
+            cubeM4[i][2] = temp[i];
+        }
+}
+
+void rotateD(string direction)
+{
+    char temp[3] = {};
+    if (direction == "D")
+        for (int i = 0; i < 3; i++)
+        {
+            temp[i] = cubeM1[2][i];
+            cubeM1[2][i] = cubeM4[2][i];
+            cubeM4[2][i] = cubeM3[2][i];
+            cubeM3[2][i] = cubeM2[2][i];
+            cubeM2[2][i] = temp[i];
+        }
+    else
+        for (int i = 0; i < 3; i++)
+        {
+            temp[i] = cubeM1[2][i];
+            cubeM1[2][i] = cubeM2[2][i];
+            cubeM2[2][i] = cubeM3[2][i];
+            cubeM3[2][i] = cubeM4[2][i];
+            cubeM4[2][i] = temp[i];
+        }
+}
+
 int main()
 {
     printCube();
